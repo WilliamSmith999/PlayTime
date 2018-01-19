ea74c734962b29799f660ff15bf027ab3b2bb055
William Smith <williamsmithtannis013@gmail.com>
2018-01-18 16:54:16 -0800

    Created PlayTime...

---
+++ PlayTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayTime
+//
+//  Created by William Smith on 1/18/18.
+//  Copyright © 2018 William Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
